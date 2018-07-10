e109967971f30e916b84e941ae8600499edc63a1
Vallar <tkalenko.yura15@gmail.com>
2018-07-10 20:03:03 +0300

    add resolve dentist, clinics, patients, purchase, specialty

--- app/templates/clinics/clinics.ctrl.js
+++ app/templates/clinics/clinics.ctrl.js
@@ -8,11 +8,11 @@
         var vm = this;
         vm.clinics = clinics;
         vm.clinic_countries = clinic_countries;
-        vm.count_clinic = null;
+        vm.count_clinic = 0;
         init();
 
         function init() {
-            if(vm.clinics && vm.clinic.length){
+            if(vm.clinics && vm.clinics.length){
                vm.count_clinic = vm.clinics.length;
             }
         }
